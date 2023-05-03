912a612a79381d77c7f66ec1a7152387f2d777a5
ANDREW <awade75009@gmail.com>
2023-05-03 17:41:35 -0500

    master

---
+++ modmail.py
@@ -0,0 +1,43 @@
+import nextcord
+from nextcord.ext import commands
+import os
+from dotenv import load_dotenv
+from pretty_help import PrettyHelp
+from loguru import logger
+load_dotenv()
+
+# this is for pretty help #
+
+ending_note = "For additional assistance, contact a moderator."
+color = 0x00FF00
+
+
+
+# the bots intents #
+intents = nextcord.Intents.all()
+intents.guild_messages = True
+bot = commands.Bot(command_prefix='!', intents=nextcord.Intents.all(),help_command=PrettyHelp())
+
+# this is for the logger #
+logger.add("logs/file.log", level="TRACE")
+
+# this is the on ready event #
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} is online.')
+    logger.info("Wadder ModMail is online.")
+
+# this is the on message event #
+@bot.event
+async def on_message(message):
+    # Log the message
+    logger.info(f"{message.author}: {message.content}")
+
+    # Process the message as usual
+    await bot.process_commands(message)
+
+# this is the start of the modmail bot #
+if __name__ == '__main__':
+    bot.load_extension('cogs.waddermodmail') 
+    bot.run(os.environ.get('DISCORD_BOT_TOKEN'))
+
