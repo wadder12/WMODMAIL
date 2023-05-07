95edb01e4702ca2eced5ae44e4c17a68fb3b0188
ANDREW <awade75009@gmail.com>
2023-05-07 21:00:33 +0000

    May 7, 2023, 4:00 PM

--- cogs/wadderbanhammer.py
+++ cogs/wadderbanhammer.py
@@ -1,6 +1,7 @@
 import asyncio
-import os
 import json
+import os
+
 import nextcord
 from nextcord.ext import commands
 import openai
@@ -9,6 +10,7 @@ import openai
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+
 class BanHammer(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -16,7 +18,8 @@ class BanHammer(commands.Cog):
         if not os.path.isfile(self.settings_file):
             with open(self.settings_file, "w") as f:
                 json.dump({}, f)
-        self.warns = {}
+        with open("data/ban_hammer_warns.json", "r") as f:
+            self.warns = json.load(f)
 
     def _load_settings(self):
         with open(self.settings_file, "r") as f:
@@ -26,28 +29,37 @@ class BanHammer(commands.Cog):
         with open(self.settings_file, "w") as f:
             json.dump(settings, f)
 
+    def _save_warns(self):
+        with open("data/ban_hammer_warns.json", "w") as f:
+            json.dump(self.warns, f)
+
     async def is_ban_hammer_enabled(self, guild_id):
         settings = self._load_settings()
         return settings.get(str(guild_id), {}).get("enabled", False)
-    
+
     async def warn_user(self, user, guild):
         user_id = str(user.id)
         if user_id not in self.warns:
             self.warns[user_id] = 0
         self.warns[user_id] += 1
+        self._save_warns()
 
         if self.warns[user_id] >= 5:
             # Temporarily ban the user for 3 days
-            await user.ban(reason="Inappropriate messages detected by Ban Hammer", delete_message_days=0)
+            await user.ban(
+                reason="Inappropriate messages detected by Ban Hammer",
+                delete_message_days=0,
+            )
             await asyncio.sleep(3 * 24 * 60 * 60)  # Wait for 3 days
             await guild.unban(user)
             self.warns[user_id] = 0
+            self._save_warns()
             return True
         return False
 
-
-
-    @nextcord.slash_command(name="banhammer", description="Enable or disable the Ban Hammer")
+    @nextcord.slash_command(
+        name="banhammer", description="Enable or disable the Ban Hammer"
+    )
     @commands.has_permissions(administrator=True)
     async def banhammer(self, interaction: nextcord.Interaction):
         settings = self._load_settings()
@@ -76,7 +88,7 @@ class BanHammer(commands.Cog):
 
         try:
             response = openai.Moderation.create(input=message.content)
-            flagged = response['results'][0]['flagged']
+            flagged = response["results"][0]["flagged"]
         except Exception as e:
             print(f"Error while calling OpenAI API: {e}")
             return
@@ -84,28 +96,41 @@ class BanHammer(commands.Cog):
         if flagged:
             banned = await self.warn_user(message.author, message.guild)
             if banned:
-                await message.channel.send(f"{message.author.name} has been temporarily banned for 3 days due to inappropriate messages.")
+                await message.channel.send(
+                    f"{message.author.name} has been temporarily banned for 3 days due to inappropriate messages.")
             else:
-                warning_embed = nextcord.Embed(title="Warning", color=nextcord.Color.red())
-                warning_embed.add_field(name="User", value=message.author.mention)
-                warning_embed.add_field(name="Warnings", value=f"{self.warns[str(message.author.id)]}/5")
-                warning_embed.set_footer(text="Reaching 5 warnings will result in a temporary ban for 3 days.")
-                await message.channel.send(embed=warning_embed)
-                try:
-                    await message.author.send(warning_embed)
-                except nextcord.errors.Forbidden:
-                    pass
-                
-                # Log the removed message to a channel
-                log_channel_id = 1086313113130909793 # Replace with the ID of the channel to log to
-                log_channel = message.guild.get_channel(log_channel_id)
-                if log_channel is not None:
-                    embed = nextcord.Embed(title="Removed Message", color=nextcord.Color.red())
-                    embed.add_field(name="Author", value=message.author.mention)
-                    embed.add_field(name="Channel", value=message.channel.mention)
-                    embed.add_field(name="Message", value=message.content, inline=False)
-                    await log_channel.send(embed=embed)
+                        warning_embed = nextcord.Embed(
+                        title="Warning", color=nextcord.Color.red()
+                        )
+                        warning_embed.add_field(name="User", value=message.author.mention)
+                        warning_embed.add_field(
+                        name="Warnings", value=f"{self.warns[str(message.author.id)]}/5"
+                        )
+                        warning_embed.set_footer(
+                        text="Reaching 5 warnings will result in a temporary ban for 3 days."
+                        )
+                        await message.channel.send(embed=warning_embed)
+            try:
+                        await message.author.send(warning_embed)
+            except nextcord.errors.Forbidden:
+                        pass
+                    
+                     # Delete the inappropriate message
+            await message.delete()
 
+            # Log the removed message to a channel
+            log_channel_id = 1086313113130909793  # Replace with the ID of the channel to log to
+            log_channel = message.guild.get_channel(log_channel_id)
+            if log_channel is not None:
+                embed = nextcord.Embed(
+                    title="Removed Message", color=nextcord.Color.red()
+                )
+                embed.add_field(name="Author", value=message.author.mention)
+                embed.add_field(name="Channel", value=message.channel.mention)
+                embed.add_field(
+                    name="Message", value=message.content, inline=False
+                )
+                await log_channel.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(BanHammer(bot))
